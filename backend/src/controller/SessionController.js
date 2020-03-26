c06b3ccc39c3d1b8ec5b696062244d5c3414d04a
Edson Pereira de Andrade <c1297937@bb.com.br>
2020-03-25 21:02:22 -0300

    Ajuste nos cors

--- backend/src/controller/SessionController.js
+++ backend/src/controller/SessionController.js
@@ -3,8 +3,8 @@ const connection = require('../database/connections');
 class SessionController {
 
     async create(request, response){
-        const {ong_id } = request.body;
-        const ong = await  connection('ongs').where('id', ong_id).select('name').first();
+        const {id } = request.body;
+        const ong = await  connection('ongs').where('id', id).select('name').first();
 
         if(!ong){
             return response.status(400).json({error: 'Ong não encontrado'});
